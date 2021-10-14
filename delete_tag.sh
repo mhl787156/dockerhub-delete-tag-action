8ffefce5011e693f5c524cd4b519976480a41a61
mickey li <mickeyhli@outlook.com>
2021-10-14 16:47:09 +0100

    Added env variabes in

--- delete_tag.sh
+++ delete_tag.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # https://devopsheaven.com/docker/dockerhub/2018/04/09/delete-docker-image-tag-dockerhub.html
 
-USERNAME="docker_username"
-PASSWORD="docker_password"
-ORGANIZATION="organization"
-IMAGE="image"
-TAG="tag"
+# USERNAME="docker_username"
+# PASSWORD="docker_password"
+# ORGANIZATION="organization"
+# IMAGE="image"
+# TAG="tag"
 
 login_data() {
 cat <<EOF
@@ -21,3 +21,5 @@ TOKEN=`curl -s -H "Content-Type: application/json" -X POST -d "$(login_data)" "h
 curl "https://hub.docker.com/v2/repositories/${ORGANIZATION}/${IMAGE}/tags/${TAG}/" \
 -X DELETE \
 -H "Authorization: JWT ${TOKEN}"
+
+echo "::set-output name=success::true"
